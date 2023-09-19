4048e85756b58ccb1fe9e254d43c76d6803307ba
yining <yiningzha.yz@gmail.com>
2023-09-19 15:17:43 -0400

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by yining zha on 19/9/23.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
